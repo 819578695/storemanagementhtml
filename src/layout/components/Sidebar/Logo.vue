b135296f61d0c37dfd36b9087cdd71356132713e
shenweikang <shenweikang@zgt56.com>
2019-10-23 13:43:10 +0800

    修改logo

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <el-menu-item v-if="sidebarLogo" :class="{'submenu-title-noDropdown':isCollapse}" index="0" style="pointer-events: none;">
     <!-- 缩小时显示的logo，可以自定义，这里直接使用图标库中的 -->
-    <svg-icon v-if="isCollapse" icon-class="run" />
+    <svg-icon v-if="isCollapse" icon-class="smalllogo" />
     <!--正常状态下显示的，可以使用本地的logoImg-->
     <div class="logo-con">
       <img src="../../../../static/img/logo.png" style=" display: inline-block;height: auto;max-width: 100%;" >
